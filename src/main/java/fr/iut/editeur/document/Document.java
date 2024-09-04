26feb915416bba6173d5eb34a292978cd900aed8
Lopes Manon <manon.lopes@etu.umontpellier.fr>
2024-09-04 16:44:26 +0200

    ajout commande maj

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -13,6 +13,15 @@ public class Document {
         String partieDroite = texte.substring(fin + 1);
         texte = partieGauche + remplacement + partieDroite;
     }
+    public void majuscules(int debut, int fin) {
+        String maj = "";
+        String partieGauche = texte.substring(0, debut);
+        String partieDroite = texte.substring(fin + 1);
+        for(int i = debut; i <= fin; i++) {
+           maj += Character.toUpperCase(texte.charAt(i));
+        }
+        texte = partieGauche + maj + partieDroite;
+    }
 	
     public String getTexte() {
         return texte;
